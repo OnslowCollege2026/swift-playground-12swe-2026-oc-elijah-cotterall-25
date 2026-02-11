6d1dc1dce5caf6202a38edfc831069d6fc12f926
oc-elijah-cotterall-25 <168697755+oc-elijah-cotterall-25@users.noreply.github.com>
2026-02-11 21:24:57 +0000

    commit

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,40 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        let maximumItemVolume = 2.0
+
+        print("Enter the room length: ")
+        guard let userInput = readLine(), let number = Double(userInput), number >= 0 else {
+            print("Invaild number.")
+            }
+            roomLength = number
+            
+            print("Enter the room Width: ")
+            guard let userInput = readLine(), let roomWidth = Double(userInput) else {
+
+            print("Enter the room height: ")
+            guard let userInput = readLine(), let roomHeight = Double(userInput) else {
+                let roomArea = roomLength * roomWidth
+                let roomVolume = roomArea * roomHeight
+
+            print("Room Area: \(roomArea) m²")
+            print("Room Volume: \(roomVolume )m³")
+
+                let furnitureVolumes = [1.2, 0.8, 2.5, 0.6, 1.0]
+
+                var totalFurnitureVolume = 0.0
+
+                    furnitureVolumes.enumerated().forEach { index, volume in
+                    print("Item \(index + 1): \(volume) m³")
+                    if volume > maximumItemVolume {
+                    print("Oversized item Detected.")
+                    }
+                    totalFurnitureVolume = totalFurnitureVolume + volume
+                }
+
+                let usableVolume = roomVolume - totalFurnitureVolume
+                print("Usable volume: \(usableVolume) m³")
+            }
+        }
     }
 }
