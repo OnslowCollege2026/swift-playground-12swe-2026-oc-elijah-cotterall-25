91d8b67a9136e0d3934ebe459fa5dd80440cdb3a
oc-elijah-cotterall-25 <168697755+oc-elijah-cotterall-25@users.noreply.github.com>
2026-03-13 01:15:20 +0000

    wip

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,43 +1,22 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//// The Swift Programming Language
+//// https://docs.swift.org/swift-book
 
 @main
 struct SwiftPlayground {
     static func main() {
-        let maximumItemVolume = 2.0
 
-        print("Enter the room length: ")
-        guard let userInput = readLine(), let number = Double(userInput), number >= 0 else {
-            print("Invaild number.")
-            }
-            roomLength = number
-            
-            print("Enter the room Width: ")
-            guard let userInput = readLine(), let roomWidth = Double(userInput) else {
+let size = 6
+var ocean = Array(repeating: Array(repeating: "~", count: size), count: size)
+var guesses = Array(repeating: Array(repeating: "~", count: size), count: size)
 
-            print("Enter the room height: ")
-            guard let userInput = readLine(), let roomHeight = Double(userInput) else {
-                let roomArea = roomLength * roomWidth
-                let roomVolume = roomArea * roomHeight
+ocean[1][3] = "S"
+ocean[2][3] = "S"
+ocean[4][0] = "S"
+ocean[5][4] = "S"
 
-            print("Room Area: \(roomArea) m²")
-            print("Room Volume: \(roomVolume )m³")
+/// Parameter:
+/// - board: The 2D grid to display.
+func printBoard(_ board: [[String]])
 
-                let furnitureVolumes = [1.2, 0.8, 2.5, 0.6, 1.0]
-
-                var totalFurnitureVolume = 0.0
-
-                    furnitureVolumes.enumerated().forEach { index, volume in
-                    print("Item \(index + 1): \(volume) m³")
-                    if volume > maximumItemVolume {
-                    print("Oversized item Detected.")
-                    }
-                    totalFurnitureVolume = totalFurnitureVolume + volume
-                }
-
-                let usableVolume = roomVolume - totalFurnitureVolume
-                print("Usable volume: \(usableVolume) m³")
-            }
-        }
     }
 }
